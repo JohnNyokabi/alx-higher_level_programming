a296454e012e807c1411e974714057d60bd0a72a
John Nyokabi <jwnyokabi@gmail.com>
2022-11-30 20:19:22 +0300

    python if, elif and else loops

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    newstr = str
     if n >= 0:
-        newstr = str[:n] + str[n + 1:]
-        return (newstr)
+        return str[:n] + str[n+1:]
+    else:
+        return str
