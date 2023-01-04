2b05f0199ab4c4bc30f6649dda6e7a0825e53256
John Nyokabi <jwnyokabi@gmail.com>
2023-01-04 08:15:05 +0300

    testing python programs

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,4 +15,4 @@ def add_integer(a, b=98):
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     else:
-        return int(a) +  int(b)
+        return int(a) + int(b)
