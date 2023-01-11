9fa66124d91bce6cdd20cd5ff75f6638d0d97827
John Nyokabi <jwnyokabi@gmail.com>
2023-01-11 18:57:43 +0300

    python input and output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python3
-"""
-Contains class Student that initializes public instance
-attributes first_name, last_name, and age, and has public
-method to_json that retrieves its dictionary representation
+""" Module that defines the class Student
 """
 
 
-class Student():
-    """Retrieves its dictionary representation"""
+class Student:
+    """ Class to create student instances """
+
     def __init__(self, first_name, last_name, age):
-        """Initializes student with full name and age"""
+        """ Special method to initialize """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """
-        Returns dictionary description with simple data structure
-        for JSON serialization of an object
-        """
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance """
+        for atr in json:
+            self.__dict__[atr] = json[atr]
