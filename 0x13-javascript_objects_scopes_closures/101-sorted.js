e1ddb5f7899879cee00c0bde6aa1f3098c8beef2
John Nyokabi <jwnyokabi@gmail.com>
2023-03-15 17:46:52 +0300

    javascript objects, scopes and closures

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const dict = require('./101-data.js').dict;
+const newDict = {};
+let key;
+for (key in dict) {
+  if (dict[key] in newDict) {
+    newDict[dict[key]].push(key);
+  } else {
+    newDict[dict[key]] = [key];
+  }
+}
+console.log(newDict);
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const parentSquare = require('./5-square');
+module.exports = class Square extends parentSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
+};
