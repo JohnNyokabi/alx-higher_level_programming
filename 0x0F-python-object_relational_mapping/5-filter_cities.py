aa75b28d3f3ea9bd0769ada40960b1f107373f1e
John Nyokabi <jwnyokabi@gmail.com>
2023-03-19 21:44:26 +0300

    python object relational mapping

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+module for the script that takes in name of a state
+and lists all cities of that state
+"""
+
+import sys
+import MySQLbd
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    query = "SELECT cities.name FROM cities"
+    query = query + " INNER JOIN states ON cities.state_id = states.id"
+    query = query + " WHERE states.name = %s ORDER BY cities.id ASC"
+    cur.execute(query, [sys.argv[4]])
+    query_rows = cur.fetchall()
+    print(", ".join([row[0] for row in query_rows]))
+    cur.close()
+    conn.close()
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Has class definition of a state and an instance Base=declarative_base()"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class inheriting from Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
