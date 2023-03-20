c0cc03080605f0f7501c5bb1ff4d3984f82453c6
John Nyokabi <jwnyokabi@gmail.com>
2023-03-20 18:25:32 +0300

    python ORM

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    query = "SELECT cities.name FROM cities"
+    query = "SELECT * FROM cities"
     query = query + " INNER JOIN states ON cities.state_id = states.id"
-    query = query + " WHERE states.name = %s ORDER BY cities.id ASC"
+    query = query + " WHERE states.name = '%s' ORDER BY cities.id ASC"
     cur.execute(query, [sys.argv[4]])
     query_rows = cur.fetchall()
     print(", ".join([row[0] for row in query_rows]))
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -2,13 +2,13 @@
 """Script that lists all State objects from the database hbtn_0e_6_usa"""
 
 import sys
-from sqlachemy import create_engine
+from sqlalchemy import create_engine
 from model_state import State
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.agv[3]), pool_pre_ping=True)
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
