9b0be23796836ac2520df047c4b140b8b2f1953a
John Nyokabi <jwnyokabi@gmail.com>
2023-04-26 21:02:51 +0300

    javascript web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,11 +5,11 @@ request(url, function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
     characters.forEach((character) => {
-	    request(character, function (error, response, body) {
+      request(character, function (error, response, body) {
         if (!error) {
-		    console.log(JSON.parse(body).name);
+          console.log(JSON.parse(body).name);
         }
-	    });
+      });
     });
   }
 });
