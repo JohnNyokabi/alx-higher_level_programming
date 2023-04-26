fecda0f341b2921b5a0b0e1294fb6570212c7d9f
John Nyokabi <jwnyokabi@gmail.com>
2023-04-26 20:19:28 +0300

    javascript web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,14 +2,14 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-    if (!error) {
-	const characters = JSON.parse(body).characters;
-	characters.forEach((character) => {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
 	    request(character, function (error, response, body) {
-		if (!error) {
+        if (!error) {
 		    console.log(JSON.parse(body).name);
-		}
+        }
 	    });
-	});
-    }
+    });
+  }
 });
